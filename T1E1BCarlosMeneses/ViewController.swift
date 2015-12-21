4ae3ca58cb51f69547eabe0449378db748538e2d
fabrica <fabrica@imac-de-futic.local>
2015-12-21 20:25:24 +0100

    Initial Commit

---
+++ T1E1BCarlosMeneses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T1E1BCarlosMeneses
+//
+//  Created by fabrica on 21/12/15.
+//  Copyright © 2015 formacion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
